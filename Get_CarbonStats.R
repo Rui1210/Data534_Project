599792b9330fe2586f253c2e568ee82467254617
zhangz128 <zhangzz1218@gmail.com>
2024-02-03 17:09:59 -0800

    test

--- Get_CarbonStats.R
+++ Get_CarbonStats.R
@@ -1,7 +1,30 @@
-library(httr)
-library(tidyverse)
-
-Get_CarbonStats <- function(from, to, block = FALSE) {
+#' Get Carbon Intensity Statistics
+#'
+#' This function retrieves carbon intensity statistics from the Carbon Intensity API
+#' for a specified date range. It returns a list containing two elements: 'data' (a
+#' data frame) and 'plot' (a ggplot object) for visualizing the intensity trends.
+#'
+#' @param from Start date in "YYYY-MM-DD" format.
+#' @param to   End date in "YYYY-MM-DD" format.
+#' @return A list with two elements: 'data' (a data frame) and 'plot' (a ggplot object).
+#'
+#' @details
+#' The function queries the Carbon Intensity API for each day within the specified
+#' date range and retrieves maximum, average, and minimum intensity values. The result
+#' is a data frame containing date-wise intensity statistics, and a ggplot object for
+#' visualizing the intensity trends over time.
+#'
+#' @examples
+#' \dontrun{
+#' Get_CarbonStats("2023-01-20", "2023-01-30")
+#' }
+#'
+#' @seealso
+#' \import{httr}
+#' \import{tidyverse}
+#'
+#' @export
+Get_CarbonStats <- function(from, to) {
   stats_url <- 'https://api.carbonintensity.org.uk/intensity/stats'
   from <- as.Date(from)
   to <- as.Date(to)
@@ -12,22 +35,7 @@ Get_CarbonStats <- function(from, to, block = FALSE) {
                           index = character(),
                           stringsAsFactors = FALSE)
   
-  if (block == TRUE){
-    for (day in seq(from, to - 1, by = '1 day')) {
-      next_day <- day + 1
-      url <- paste(stats_url, as.Date(day), as.Date(next_day), block, sep = '/')
-      response <- GET(url)
-      parsed_data <- content(response, "parsed")
-      df <- parsed_data[["data"]][[1]][["intensity"]]
-      result_row <- c(date = as.character(as.Date(day)),
-                      max = as.character(df$max),
-                      average = as.character(df$average),
-                      min = as.character(df$min),
-                      index = as.character(df$index))
-      result_df <- bind_rows(result_df, result_row)
-    }
-  }else{
-    for (day in seq(from, to - 1, by = '1 day')) {
+  for (day in seq(from, to - 1, by = '1 day')) {
       next_day <- day + 1
       url <- paste(stats_url, as.Date(day), as.Date(next_day), sep = '/')
       response <- GET(url)
@@ -39,7 +47,7 @@ Get_CarbonStats <- function(from, to, block = FALSE) {
                       min = as.character(df$min),
                       index = as.character(df$index))
       result_df <- bind_rows(result_df, result_row)
-  }}
+  }
   result_df$date <- as.Date(result_df$date, formate = "%y-%m-%d")
   result_df$max <- as.numeric(result_df$max)
   result_df$average <- as.numeric(result_df$average)
