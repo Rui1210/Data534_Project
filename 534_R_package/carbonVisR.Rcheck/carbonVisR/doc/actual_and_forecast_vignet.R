eba389d2aff609090713b3950129c25535f639ba
Zheng Zhang <zhangzz1218@gmail.com>
2024-02-05 12:19:39 -0800

    build

---
+++ 534_R_package/carbonVisR.Rcheck/carbonVisR/doc/actual_and_forecast_vignet.R
@@ -0,0 +1,13 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(carbonVisR)
+
+## ----validate_date_range------------------------------------------------------
+# Example Usage
+actual_and_forecast("2022-01-01")
+
